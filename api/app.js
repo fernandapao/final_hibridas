72b57d769b4c8a129ca0e0f82b317e92bc2f4601
fernandapao <fernandapao_@hotmail.com>
2024-12-16 15:42:44 -0300

    cambios

--- api/app.js
+++ api/app.js
@@ -22,7 +22,8 @@ const app = express();
 
 //para error de cors
 const options = {
-    origin: 'http://localhost:5173',
+    // origin: 'http://localhost:5173',
+    origin: 'https://final-hibridas-fmps.vercel.app/',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     AllowedHeathers: ['Content-Type', 'Authorization'],
     credentials: true
